765d2c2a6d81142b685d2e65e1e05dd61c70e9c5
stupid58fly <stupid58fly@gmail.com>
2016-04-20 23:40:29 +0300

    работа со статистикой игрока и его настройками через потоки ввода и вывода

---
+++ Maze/src/statistics/Settings.java
@@ -0,0 +1,190 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statistics;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.InputStream;
+import javafx.scene.paint.Color;
+
+/**
+ *
+ * @author ilyab
+ */
+public class Settings {
+    private Color wallColor;
+    private Color travelColor;
+    private Color startColor;
+    private Color finishColor;
+    private Color distanceTravelColor;
+    
+    /**
+     * Конструктор поумолчанию. Инициализирует все поля значением null.
+     */
+    public Settings() {
+        this.wallColor = null;
+        this.travelColor = null;
+        this.startColor = null;
+        this.finishColor = null;
+        this.distanceTravelColor = null;
+    }
+    
+    /**
+     * Конструктор копирования. Копирует объект ob.
+     * @param ob копируемый объект.
+     */
+    public Settings(final Settings ob) {
+        this.wallColor = ob.wallColor;
+        this.travelColor = ob.travelColor;
+        this.startColor = ob.startColor;
+        this.finishColor = ob.finishColor;
+        this.distanceTravelColor = ob.distanceTravelColor;
+    }
+    
+    /**
+     * Конструктор инициализации.
+     * @param wall цвет стен.
+     * @param travel цвет дорожек.
+     * @param start цвет старта.
+     * @param finish цвет финиша.
+     * @param distanceTravel цвет пройденного пути.
+     */
+    public Settings(final Color wall, final Color travel, final Color start, final Color finish, final Color distanceTravel) {
+        this.wallColor = wall;
+        this.travelColor = travel;
+        this.startColor = start;
+        this.finishColor = finish;
+        this.distanceTravelColor = distanceTravel;
+    }
+    
+    /**
+     * Конструктор инициализации из потока ввода. (Десериализация)
+     * @param in поток ввода, содержащий в себе информацию о настройках в формате Map.
+     * @throws java.io.IOException если информация в потоке не соответствует требованиям или с потоком что-то случилось.
+     */
+    public Settings(final InputStream in) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        String key;
+        String value;
+        int c = 0;
+        while(c >= 0) {
+            for(c = in.read(); c != '=' && c >= 0;)
+                if (c < 0 ) throw new IOException();
+                else sb.append((char)c);
+            key = sb.toString();
+            sb.delete(0, sb.length());
+            
+            for(c = in.read(); c != '\n' && c >= 0;)
+                sb.append((char)c);
+            value = sb.toString();
+            sb.delete(0, sb.length());
+            
+            switch(key) {
+                case "wallColor=":
+                    wallColor = Color.valueOf(value);
+                    break;
+                case "travelColor=":
+                    travelColor = Color.valueOf(value);
+                    break;
+                case "startColor=":
+                    startColor = Color.valueOf(value);
+                    break;
+                case "finishColor=":
+                    finishColor = Color.valueOf(value);
+                    break;
+                case "distanceTravelColor=":
+                    distanceTravelColor = Color.valueOf(value);
+                    break;
+            }
+        }
+    }
+    
+    /**
+     * Выгрузка класса в поток вывода. (Сериализация)
+     * @param out поток, в который выгружается информация о настройках в формате Map.
+     * @throws java.io.IOException если что-то случилось с потоком вывода.
+     */
+    public void print(final OutputStream out) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("wallColor=").append(wallColor).append('\n');
+        sb.append("travelColor=").append(travelColor).append('\n');
+        sb.append("startColor=").append(startColor).append('\n');
+        sb.append("finishColor=").append(finishColor).append('\n');
+        sb.append("distanceTravelColor=").append(distanceTravelColor);
+        out.write(sb.toString().getBytes());
+        out.flush();
+    }
+
+    /**
+     * @return цвет стен.
+     */
+    public Color getWallColor() {
+        return wallColor;
+    }
+
+    /**
+     * @param wallColor цвет стен для задания.
+     */
+    public void setWallColor(Color wallColor) {
+        this.wallColor = wallColor;
+    }
+
+    /**
+     * @return цвет дорожек.
+     */
+    public Color getTravelColor() {
+        return travelColor;
+    }
+
+    /**
+     * @param travelColor цвет дорожек для задания.
+     */
+    public void setTravelColor(Color travelColor) {
+        this.travelColor = travelColor;
+    }
+
+    /**
+     * @return цвет старта.
+     */
+    public Color getStartColor() {
+        return startColor;
+    }
+
+    /**
+     * @param startColor цвет старта для задания.
+     */
+    public void setStartColor(Color startColor) {
+        this.startColor = startColor;
+    }
+
+    /**
+     * @return цвет финиша.
+     */
+    public Color getFinishColor() {
+        return finishColor;
+    }
+
+    /**
+     * @param finishColor цвет финиша для задания.
+     */
+    public void setFinishColor(Color finishColor) {
+        this.finishColor = finishColor;
+    }
+
+    /**
+     * @return цвет пройденого пути.
+     */
+    public Color getDistanceTravelColor() {
+        return distanceTravelColor;
+    }
+
+    /**
+     * @param distanceTravelColor цвет пройденого пути для задания.
+     */
+    public void setDistanceTravelColor(Color distanceTravelColor) {
+        this.distanceTravelColor = distanceTravelColor;
+    }
+}
---
+++ Maze/src/statistics/Statistics.java
@@ -0,0 +1,160 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statistics;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author ilyab
+ */
+public class Statistics {
+    private String levelName;
+    private Long time;
+    private Long step;
+    private Long helpAmount;
+    
+    /**
+     * Конструктор поумолчанию инициализирует все поля значениями null.
+     */
+    public Statistics() {
+        this.levelName = null;
+        this.time = null;
+        this.step = null;
+        this.helpAmount = null;
+    }
+    
+    /**
+     * Конструктор инициализации.
+     * @param levelName значение названия уровня для задания.
+     * @param time значение времени прохождения уровня для задания.
+     * @param step значение количества шагов для задания.
+     * @param helpAmount зачение количества подсказок для задания.
+     */
+    public Statistics(final String levelName, final Long time, final Long step, final Long helpAmount) {
+        this.levelName = levelName;
+        this.time = time;
+        this.step = step;
+        this.helpAmount = helpAmount;
+    }
+    
+    /**
+     * Конструктор копирования.
+     * @param ob объект для копирования.
+     */
+    public Statistics(final Statistics ob) {
+        this.levelName = ob.levelName;
+        this.time = ob.time;
+        this.helpAmount = ob.helpAmount;
+    }
+    
+    /**
+     * Конструктор инициализации из потока. конструктор десериализации.
+     * @param in поток, в котором хранится информация о статистике в формате Map.
+     * @throws IOException если впроблемы с потоком, или информация в нем не соответствует заданным требованиям.
+     */
+    public Statistics(final java.io.InputStream in) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        String key;
+        String value;
+        int c = 0;
+        while(c >= 0) {
+            sb.delete(0, sb.length());
+            for (c = in.read(); c != '=' && c >= 0;)
+                sb.append((char)c);
+            key = sb.toString();
+            
+            sb.delete(0, sb.length());
+            for (c = in.read(); c != '\n' && c >= 0;)
+                sb.append((char)c);
+            value = sb.toString();
+            
+            switch(key) {
+                case "levelName":
+                    levelName = value;
+                    break;
+                case "time":
+                    time = Long.parseLong(value);
+                    break;
+                case "helpAmount":
+                    helpAmount = Long.parseLong(value);
+                    break;
+            }
+        }
+            
+    }
+    
+    /**
+     * Десериализация.
+     * @param out поток вывода, в который кладётся десиреализованный объект.
+     * @throws IOException если что-то с потоком случилось не так.
+     */
+    public void print(java.io.OutputStream out) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("levelName=").append(levelName).append('\n');
+        sb.append("time=").append(time.toString()).append('\n');
+        sb.append("step").append(getStep()).append('\n');
+        sb.append("helpAmount=").append(helpAmount.toString());
+        out.write(sb.toString().getBytes());
+        out.flush();
+    }
+
+    /**
+     * @return имя уровня.
+     */
+    public String getLevelName() {
+        return levelName;
+    }
+
+    /**
+     * @param levelName имя уровня для задания.
+     */
+    public void setLevelName(String levelName) {
+        this.levelName = levelName;
+    }
+
+    /**
+     * @return время прохождения уровня.
+     */
+    public Long getTime() {
+        return time;
+    }
+
+    /**
+     * @param time время прохождения уровня для задания.
+     */
+    public void setTime(Long time) {
+        this.time = time;
+    }
+
+    /**
+     * @return число использованных подсказок.
+     */
+    public Long getHelpAmount() {
+        return helpAmount;
+    }
+
+    /**
+     * @param helpAmount число использованных подсказок для задания.
+     */
+    public void setHelpAmount(Long helpAmount) {
+        this.helpAmount = helpAmount;
+    }
+
+    /**
+     * @return число шагов, за которые был пройден лабиринт.
+     */
+    public Long getStep() {
+        return step;
+    }
+
+    /**
+     * @param step число шагов, за которые был пройден лабиринт, для задания.
+     */
+    public void setStep(Long step) {
+        this.step = step;
+    }
+}
